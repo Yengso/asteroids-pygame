1c4a82a61c0fb2c2662ae793d5c433f05ea9b3bd
Yengso <sondre.engstrom@hotmail.com>
2025-08-16 23:52:16 +0200

    add max 60fps clock object

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ def main():
     pygame.display.set_mode()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock_object = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        clock_object.tick(60)
+        dt = clock_object.tick(60) / 1000
 
     print("Starting Ateroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
