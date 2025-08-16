2304ab8f3d840a3d456ea130890167bce41dc660
Yengso <sondre.engstrom@hotmail.com>
2025-08-16 23:30:18 +0200

    add display

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import pygame
+from constants import *
+
+
+
 def main():
-    print("Hello from asteroids!")
-    print
+    pygame.init()
+    pygame.display.set_mode()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+    print("Starting Ateroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    PRINT(f"Screen height: {SCREEN_HEIGHT}")
 if __name__ == "__main__":
     main()
