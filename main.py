4ebd5f488e9d3e2d44832434c7054fe7f9c84c77
Yengso <sondre.engstrom@hotmail.com>
2025-08-18 21:50:23 +0200

    finish player object

--- main.py
+++ main.py
@@ -8,25 +8,33 @@ def main():
     pygame.init()
     pygame.display.set_mode()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Ateroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
 
     clock_object = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for drawables in drawable:
+            drawables.draw(screen)
         pygame.display.flip()
         clock_object.tick(60)
         dt = clock_object.tick(60) / 1000
 
-    print("Starting Ateroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    PRINT(f"Screen height: {SCREEN_HEIGHT}")
+    
 if __name__ == "__main__":
     main()
